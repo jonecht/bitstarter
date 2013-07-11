f62f8824eb2b53d0b921ff768dc195fb6cae59e9
Jon Echt <jonecht@gmail.com>
2013-07-11 02:14:29 +0000

    removed buffer

--- web.js
+++ web.js
@@ -4,10 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs =require('fs');
-    hText = new Buffer(256);
-    hText.write(fs.readFileSync('index.html','utf-8'));
-    
-    response.send(hText.toString('utf8'));
+     response.send(fs.readFileSync('index.html','utf-8'));
 });
 
 var port = process.env.PORT || 5000;
